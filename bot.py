5c92ec852355d6a23ffc70a7d8dfb6915c9f888c
OnlyNoco <abidabdullah.cloud@gmail.com>
2025-12-03 14:00:07 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
-from config import LOGGER, PORT, OWNER_ID, SHORT_URL, SHORT_API, SHORT_TUT
+from config import LOGGER, PORT, OWNER_ID, SHORT_URL, SHORT_API, SHORT_TUT, DB_URI, DB_NAME
 from helper import MongoDB
 
 version = "v1.0.0"
