808927be4a1453c33f85eb15ae2dd965c166d18b
OnlyNoco <abidabdullah.cloud@gmail.com>
2026-01-04 20:06:47 +0600

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,6 @@ TOKEN = os.environ.get("TOKEN", "0")
 API_ID = "26254064"
 API_HASH = "72541d6610ae7730e6135af9423b319c"
 WORKERS = 5 
-ON_SHORT_URL = os.environ.get("ON_SHORT_URL", "https://hentaicrispshorts.vercel.app")
 
 DB_URI = os.environ.get("DB_URI", "ITS USING CONFIG DB ")
 DB_NAME = "Cluster0"
@@ -49,11 +48,9 @@ MESSAGES = {
     "ABOUT": "<b>›› ᴀʙᴏᴜᴛ ᴍᴇ:<blockquote expandable>◧ ᴏᴡɴᴇʀ: <a href='https://t.me/OnlyNoco'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n◧ ᴏᴜʀ sᴇʀᴠɪᴄᴇs ~\n⊡ <a href='https://t.me/HeavenlySubs'>ʙᴀᴛᴛʟᴇ ᴛʜʀᴏᴜɢʜ ᴛʜᴇ ʜᴇᴀᴠᴇɴs</a>\n⊡ <a href='https://t.me/FinishedAnimeCrisp'>ꜰɪɴɪsʜᴇᴅ ᴀɴɪᴍᴇ</a>\n⊡ <a href='https://t.me/CrispIndex'>ᴀɴɪᴍᴇ ɪɴᴅᴇx</a>\n⊡ <a href='https://t.me/+O7PeEMZOAoMzYzVl'>ʜᴇɴᴛᴀɪ ᴄʀɪsᴘ</a>\n⊡ <a href='https://t.me/DisscusionAnimeCrisp'>ᴅɪsᴄᴜssɪᴏɴ ɢʀᴏᴜᴘ</a>\n\n♧ ᴊᴏɪɴ ᴜs ꜰᴏʀ ᴏʀɢᴀɴɪᴢᴇᴅ ᴍᴜʟᴛɪ ǫᴜᴀʟɪᴛʏ ᴀɴɪᴍᴇ ᴄᴏɴᴛᴇɴᴛ ♧</blockquote></b>",
     "REPLY": "<b>›› ᴊᴏɪɴ ᴜs ꜰᴏʀ ᴍᴏʀᴇ - <a href='https://t.me/FinishedAnimeCrisp'>ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴠɪsɪᴛ</a></b>",
     "SHORT_MSG": "<b>📊 ʜᴇʏ {first},\n\n‼️ ᴛʜɪꜱ ꜱʜᴏʀᴛɴᴇʀ ɪꜱ ᴍᴀᴅᴇ ᴍʏ ᴍᴇ ꜱᴏ ᴍᴀx 6 ꜱᴇᴄᴏɴᴅꜱ ᴡᴀɪᴛ ᴀɴᴅ ᴀ ᴄʟɪᴄᴋ ɴᴏᴛʜɪɴɢ ᴇʟꜱᴇ ʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ᴅᴏ ᴛᴏ ɢᴇᴛ ᴛʜᴇ ꜰɪʟᴇ ꜱᴏ, ᴡʜʏ ɴᴏᴛ ɢɪᴠᴇ ɪᴛ ᴀ ꜱʜᴏᴛ</b>",
-    "ON_SHORT_MSG": "<b>📊 ʜᴇʏ {first},\n\n‼️ ɢᴇᴛ ᴀʟʟ ꜰɪʟᴇs ɪɴ ᴀ sɪɴɢʟᴇ ʟɪɴᴋ ‼️\n\n⌯ ʏᴏᴜʀ ʟɪɴᴋ ɪs ʀᴇᴀᴅʏ — ᴛᴀᴘ ᴏɴ ‘ᴏᴘᴇɴ ʟɪɴᴋ’ ᴛᴏ ᴠɪᴇᴡ.</b>",
     "START_PHOTO": "https://envs.sh/FVk.jpg",
     "FSUB_PHOTO": "https://envs.sh/-Dw.jpg",
     "SHORT_PIC": "https://telegra.ph/file/7a16ef7abae23bd238c82-b8fbdcb05422d71974.jpg",
-    "ON_SHORT_PIC": "https://envs.sh/-s2.jpg",
     "SHORT": "https://telegra.ph/file/8aaf4df8c138c6685dcee-05d3b183d4978ec347.jpg"
 }
 
